e059be92e0261b993f0e6f6a7ae1685f53fb7c32
reinvantveer <rein@vantveer.me>
2022-12-01 11:39:29 +0100

    validate URIs in sample graphs

--- validate.py
+++ validate.py
@@ -1,9 +1,12 @@
 import os
+from urllib.request import urlopen
 
 import pyshacl
+import rdflib
 
 
 def main() -> int:
+    check_links()
     shacl_validate()
 
     return 0
@@ -22,6 +25,25 @@ def shacl_validate():
             shacl_graph=os.path.join('shacl', sample_file),
         )
         assert conforms, f'{sample_file} incorrect: {results_text}'
+
+
+def check_links() -> None:
+    """
+    This function will check _every_ URI in the sample graphs for being able to resolve. This will catch errors on
+    vocab typos, or small happy accidents on hash/slash-URI mishaps and the likes.
+
+    :return: None
+    """
+    data_samples = os.listdir('voorbeelddata')
+
+    for sample_file in data_samples:
+        graph = rdflib.Graph()
+        graph.parse(os.path.join('voorbeelddata', sample_file))
+
+        for (subject, predicate, object_) in graph:
+            if isinstance(subject, rdflib.URIRef):
+                # urlopen will throw an error on 400 or larger responses
+                urlopen(subject).read()
 
 
 if __name__ == '__main__':
