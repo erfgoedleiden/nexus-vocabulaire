b31f0088101fb498fc1d0c104051cfc51cfebcce
reinvantveer <rein@vantveer.me>
2022-12-01 11:18:42 +0100

    auto-load corresponding shacl definition assuming same file name

--- validate.py
+++ validate.py
@@ -4,16 +4,24 @@ import pyshacl
 
 
 def main() -> int:
-    data_samples = os.listdir('voorbeelddata')
+    shacl_validate()
 
+    return 0
+
+
+def shacl_validate():
+    """
+    Validates the data samples against the data shape constraint definitions
+
+    :return: None
+    """
+    data_samples = os.listdir('voorbeelddata')
     for sample_file in data_samples:
         conforms, results_graph, results_text = pyshacl.validate(
             data_graph=os.path.join('voorbeelddata', sample_file),
-            shacl_graph='some_shacl_file_location',
+            shacl_graph=os.path.join('shacl', sample_file),
         )
         assert conforms, f'{sample_file} incorrect: {results_text}'
-
-    return 0
 
 
 if __name__ == 'main':
